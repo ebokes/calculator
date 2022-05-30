c6e28f6925b231c32e19dafea486f024503150f4
Egbuchulam Chibuokem <Buokem@Buokems-MacBook-Pro.local>
2022-05-30 17:20:33 +0100

    style: adjust width, margin and border radius of calculator layout

--- src/components/layout/layoutStyle.js
+++ src/components/layout/layoutStyle.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 export const LayoutWrapper = styled.div`
-  width: 500px;
-  margin: 0 auto;
+  width: 375px;
+  margin: 40px auto 0 auto;
   padding: 20px;
   background-color: #303030;
+  border-radius: 20px;
 `;
