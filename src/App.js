0b8c59709c8778f98aef9246409660760f4a685d
Egbuchulam Chibuokem <Buokem@Buokems-MacBook-Pro.local>
2022-05-30 17:02:08 +0100

    feat: create theme colors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { ThemeProvider } from "styled-components";
 import "./App.css";
 import Layout from "./components/layout/Layout";
 
+const theme = {
+  color: {
+    body: "#17181A",
+    button: {
+      special: {
+        bg: "#616161",
+        fg: "#A5A5A5",
+      },
+      operator: {
+        bg: "#005DB2",
+        fg: "#B2DAFF",
+      },
+      digits: {
+        bg: "#303136",
+        fg: "#29A8FF",
+      },
+    },
+  },
+};
+
 function App() {
   return (
-    <div className="App">
-      <Layout />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Layout />
+      </div>
+    </ThemeProvider>
   );
 }
 
