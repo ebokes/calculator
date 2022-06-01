6223e3f8a0d362029557faa73df521d20600001d
Egbuchulam Chibuokem <Buokem@Buokems-MacBook-Pro.local>
2022-06-01 10:23:56 +0100

    style: adjust visual appearance of calculator and add animation to button clicks

--- src/components/button/buttonStyle.js
+++ src/components/button/buttonStyle.js
@@ -19,9 +19,16 @@ export const Button = styled.div`
     border: none;
     background-color: ${({ bg }) => bg || "#191a1f"};
     color: ${({ fg }) => fg || "#586063"};
-    box-shadow: -6px -6px 5px -1px #ffffff1f;
+    box-shadow: 6px 8px 3px 1px #06070b;
+    position: relative;
+    transition: 0.2s;
+
+    &:active {
+      box-shadow: -4px -4px 5px -1px #06070b;
+      transform: translate(-2px, -2px);
+    }
   }
 
-  box-shadow: 6px 8px 3px 1px #06070b;
+  box-shadow: -6px -6px 5px -1px #ffffff1f;
   border-radius: 10px;
 `;
